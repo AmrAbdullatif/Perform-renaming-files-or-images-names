e35a86a0e625481bc9feecce350ec3e69020d302
AmrAbdullatif <31695791+AmrAbdullatif@users.noreply.github.com>
2018-10-15 12:11:13 +0200

    Create rename_files.py

---
+++ rename_files.py
@@ -0,0 +1,27 @@
+def rename_files(path, new_name, file_extension):
+    """
+        Perform renaming imges names
+
+        Parameters
+        ----------
+        path : str 
+            path to your images directory
+        new_name : str
+            new name for an image
+        file_extension : str
+            extension format
+    """
+    dirs = os.listdir( path )
+    for i,file in enumerate(dirs):
+        try:
+            os.rename(path + "/" + file, path + "/" + new_name + '.' + str(i) + ".jpg")
+            # print (i, file)
+        except OSError as err:
+            print("OS error: {0}".format(err))
+            break
+
+# Rename all the jpg images to a "you_new_name" + a number
+# Example
+
+### f_dir = 'C:/images'
+### rename_files(f_dir, 'you_new_name', '.jpg')
